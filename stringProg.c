bef754cae5d3c561327153062d8f7f25662ac985
dvirBiton1 <dvir199710@gmail.com>
2021-12-15 14:02:51 +0200

    numerology

--- stringProg.c
+++ stringProg.c
@@ -1,19 +1,33 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdio.h>
+#include <ctype.h>
 #include "stringProg_lib.h"
 #define TXT 1024
 #define WORD 30
+
+int endLine(char c)
+{
+    if (c == ' ' || c == '\t' || c == '\n')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 
 int equal(char c)
 {
     int equal = 0;
     if (c >= 'a' && c <= 'z')
     {
-        equal += c - 96;
+        equal += (c - 96);
     }
     else if (c >= 'A' && c <= 'Z')
     {
-        equal += c - 64;
+        equal += (c - 64);
     }
     return equal;
 }
@@ -28,9 +42,31 @@ int numerology(char *s)
     }
     return sum;
 }
+int numerology2(char *s1, char *s2)
+{
+    int sum = 0;
+    while (s1 != s2)
+    {
+        sum += equal(*s1);
+        s1++;
+    }
+    sum += *s1;
+    return sum;
+}
+int numerology3(char *txt, int index1, int index2)
+{
+    int sum = 0;
+    while (index1 != index2)
+    {
+        sum += equal(txt[index1]);
+        index1++;
+    }
+    sum += txt[index1];
+    return sum;
+}
 int isAlpha(char c)
 {
-    if ((c > '`' && c < '{') || (c > '@' && c < '[') || (c == ' '))
+    if ((c >= 'a' && c <= 'z') || ('A' <= c && c <= 'Z'))
     {
         return 1;
     }
@@ -41,46 +77,65 @@ int isAlpha(char c)
 }
 void Afunction(char *word, char *text)
 {
-    char temp[30] = {0};
-    int len = strlen(text);
-    int j = 0;
-    for (int i = 0; i <= len; i++)
+    printf("Gematria Sequences: ");
+    int numerologyWord = numerology(word);
+    int start = 0;
+    int startWord = 0;
+    int flag = 0;
+    while (text[start])
     {
-        if (isAlpha(text[i]))
+        int sum = 0;
+        startWord = start;
+        if (isAlpha(text[start]))
         {
-            temp[j++] = text[i];
-        }
-        else
-        {   
-            int l = strlen(temp);
-            for (int i = 0; i <= l; i++)
+            while (text[startWord] && sum < numerologyWord)
             {
-                if (numerology(word) == numerology(&temp[i]))
+                sum += equal(text[startWord]);
+                startWord++;
+            }
+            --startWord;
+            if (sum == numerologyWord)
+            {
+                if (flag)
                 {
-                    printf("%s\n", &temp[i]);
+                    printf("~");
+                }
+                flag = 1;
+                for (int i = start; i <= startWord; i++)
+                {
+                    printf("%c", text[i]);
                 }
             }
-            memset(temp, 0, sizeof(temp));
-                j = 0;
         }
+        start++;
     }
+    printf("\n");
 }
+
 int main()
 {
     char word[WORD] = {0};
     char text[TXT] = {0};
     scanf("%s", word);
-    printf("%s\n", word);
     char ch = 0;
     int i = 0;
     ch = getchar();
+    while (!endLine(ch))
+    {
+        word[i] = ch;
+        ch = getchar();
+    }
+    i = 0;
+    ch = getchar();
     do
     {
-        text[i++] = ch;
+        text[i] = ch;
+        i++;
         ch = getchar();
     } while (ch != '~');
+    printf("%s\n", word);
     printf("%s\n", text);
-    printf("***Afuncion***\n");
     Afunction(word, text);
+    // funcA(text,word);
     return 0;
 }
